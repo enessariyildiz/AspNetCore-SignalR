3aa6d3bd20de099b357750008a851f4e2ad8ae41
Mithaverse <ens.sryldz@gmail.com>
2024-03-11 17:50:18 +0300

    client: signalr library loaded

--- SignalR.Web/obj/Debug/net8.0/SignalR.Web.AssemblyInfo.cs
+++ SignalR.Web/obj/Debug/net8.0/SignalR.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a6d287b7de9e966687a51fae458f8d4710b7d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
