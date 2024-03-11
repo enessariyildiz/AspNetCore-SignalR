86a6d287b7de9e966687a51fae458f8d4710b7d2
Mithaverse <ens.sryldz@gmail.com>
2024-03-11 17:13:26 +0300

    first commit

---
+++ SignalR.Web/Hubs/ExampleHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Web.Hubs
+{
+    public class ExampleHub : Hub
+    {
+        public async Task BroadCastMessageToAllClient(string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessageForAllClient", message);
+        }
+    }
+}
