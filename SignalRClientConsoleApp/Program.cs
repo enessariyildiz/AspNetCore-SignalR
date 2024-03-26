5f16ffe2d7bcd988203f8e34b6bf8440c40d6199
Mithaverse <ens.sryldz@gmail.com>
2024-03-26 16:56:30 +0300

    Console app SignalR client-1

---
+++ SignalRClientConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalRClientConsoleApp;
+
+Console.WriteLine("SignalR Console Client");
+
+var connection = new HubConnectionBuilder().WithUrl("https://localhost:7008/exampleTypeSafeHub").Build();
+
+connection.StartAsync().ContinueWith((result) =>
+{
+    Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Connection failed");
+});
+
+connection.On<Product>("ReceiveTypedMessageForAllClient", (product) =>
+{
+    Console.WriteLine($"Receive message: {product.Id}-{product.Name}-{product.Price}");
+});
+
+Console.ReadKey();
+
